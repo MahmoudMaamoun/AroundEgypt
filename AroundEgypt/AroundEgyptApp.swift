4b7be3197368aaf596e3847b2da1efe461ddec4b
Mahmoud Maamoun <maamounsharaky@gmail.com>
2024-12-28 15:40:05 +0200

    Initial Commit

---
+++ AroundEgypt/AroundEgyptApp.swift
@@ -0,0 +1,20 @@
+//
+//  AroundEgyptApp.swift
+//  AroundEgypt
+//
+//  Created by Mahmoud Maamoun on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
